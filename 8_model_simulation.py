61d0b67fc2406d48dc20c60f7f888d79ee6e5a11
Jeff Fletcher <fletch.jeff@gmail.com>
2020-10-15 20:36:19 +0200

    Update 8_model_simulation.py

--- 8_model_simulation.py
+++ 8_model_simulation.py
@@ -13,7 +13,7 @@ import time
 # Get the model id from the model you deployed in step 5. These are unique to each 
 # model on CML.
 
-model_id = "16"
+model_id = "268"
 
 flights_data_df = pd.read_csv("data/all_flight_data.csv")
 
@@ -72,41 +72,3 @@ while True:
   accuracy = classification_report(actual_result,predicted_result,output_dict=True)['accuracy']
   cdsw.track_aggregate_metrics({"accuracy": accuracy}, start_time_ms , end_time_ms, model_deployment_crn=Deployment_CRN)
   print("adding accuracy measure of" + str(accuracy))
-
-  
-
-#for record in json.loads(df_sample_clean.to_json(orient='records')):
-#  print("Added {} records".format(percent_counter)) if (percent_counter%50 == 0) else None
-#  percent_counter += 1
-#  no_churn_record = copy.deepcopy(record)
-#  no_churn_record.pop('customerID')
-#  no_churn_record.pop('Churn')
-#  # **note** this is an easy way to interact with a model in a script
-#  response = cdsw.call_model(latest_model["accessKey"],no_churn_record)
-#  response_labels_sample.append(
-#    {
-#      "uuid":response["response"]["uuid"],
-#      "final_label":churn_error(record["Churn"],percent_counter/percent_max),
-#      "response_label":response["response"]["prediction"]["probability"] >= 0.5,
-#      "timestamp_ms":int(round(time.time() * 1000))
-#    }
-#  )
-#
-#  
-#  # The "ground truth" loop adds the updated actual label value and an accuracy measure
-## every 100 calls to the model.
-#for index, vals in enumerate(response_labels_sample):
-#  print("Update {} records".format(index)) if (index%50 == 0) else None  
-#  cdsw.track_delayed_metrics({"final_label":vals['final_label']}, vals['uuid'])
-#  if (index%100 == 0):
-#    start_timestamp_ms = vals['timestamp_ms']
-#    final_labels = []
-#    response_labels = []
-#  final_labels.append(vals['final_label'])
-#  response_labels.append(vals['response_label'])
-#  if (index%100 == 99):
-#    print("Adding accuracy metrc")
-#    end_timestamp_ms = vals['timestamp_ms']
-#    accuracy = classification_report(final_labels,response_labels,output_dict=True)["accuracy"]
-#    cdsw.track_aggregate_metrics({"accuracy": accuracy}, start_timestamp_ms , end_timestamp_ms, model_deployment_crn=Deployment_CRN)
-#
